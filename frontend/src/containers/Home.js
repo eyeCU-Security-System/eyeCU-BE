8ce0a0d2bf532b3fb62682e3eb5068f04d6be02e
jawnhoang <johnhoang5@gmail.com>
2022-01-26 18:59:10 -0800

    1/26/22 frontend files

---
+++ frontend/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home(){
+    return(
+        <div className = "Home">
+            <div className = "lander">
+                <h1>eyeCU</h1>
+                <p className = "text-muted">Take Control Of Who Comes</p>
+            </div>
+        </div>
+    );
+}
